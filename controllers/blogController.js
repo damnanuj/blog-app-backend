44ab70996ffaa07574b3184eacd785dea3d2af57
Anuj <anujkrgupta21@gmail.com>
2024-07-17 21:41:14 +0530

    edit blog, delete blog api done

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,7 +1,16 @@
 const { blogDataValidator } = require("../utills/blogUtills");
 
-const { createBlog, getAllBlogs } = require("../models/blogModel");
+const {
+  createBlog,
+  getAllBlogs,
+  getmyBlogs,
+  getBlogWithId,
+  editBlog,
+  deleteBlog,
+} = require("../models/blogModel");
+const blogSchema = require("../schemas/blogSchema");
 
+//=================Create Blogs Controller
 const createBlogController = async (req, res) => {
   const { title, textBody } = req.body;
   const userId = req.session.user.userId;
@@ -34,18 +43,17 @@ const createBlogController = async (req, res) => {
   }
 };
 
-//get all blogs
+//==============get all blogs Controller
 
 const getBlogsController = async (req, res) => {
-  const SKIP = req.query.skip || 0;
+  const SKIP = parseInt(req.query.skip) || 0;
 
   try {
-    const blogsDb = await getAllBlogs({ SKIP });
-
+    const allBlogsDb = await getAllBlogs({ SKIP });
     return res.send({
       status: 200,
       message: "Read allblogs successfull",
-      data: blogsDb,
+      data: allBlogsDb,
     });
   } catch (error) {
     return res.send({
@@ -56,4 +64,114 @@ const getBlogsController = async (req, res) => {
   }
 };
 
-module.exports = { createBlogController, getBlogsController };
+//==============my blogscontroller
+const getMyBlogsController = async (req, res) => {
+  const userId = req.session.user.userId;
+  const SKIP = parseInt(req.query.skip) || 0;
+
+  try {
+    const myblogsDb = await getmyBlogs({ userId, SKIP });
+
+    if (myblogsDb.length < 1) {
+      return res.send({
+        status: 204,
+        message: "No more blogs found",
+      });
+    }
+    return res.send({
+      status: 200,
+      message: "Read my blogs success",
+      data: myblogsDb,
+    });
+  } catch (error) {
+    return res.send({
+      status: 500,
+      message: "Internal sever error",
+      error: error,
+    });
+  }
+};
+
+// ===========Edit blog Controller
+const editBlogController = async (req, res) => {
+  const { title, textBody, blogId } = req.body;
+  const userId = req.session.user.userId;
+  //data Validation
+  try {
+    await blogDataValidator({ title, textBody });
+  } catch (error) {
+    return res.send({
+      status: 400,
+      message: "Invalid data",
+      error: error,
+    });
+  }
+
+  //find the blog
+  try {
+    const blogDb = await getBlogWithId({ blogId });
+    //ownership check
+    if (!userId.equals(blogDb.userId)) {
+      return res.send({
+        status: 403,
+        message: "You are not allowed to edit",
+      });
+    }
+    //update/edit the blog
+    const blogPrevDb = await editBlog({ title, textBody, blogId });
+    return res.send({
+      status: 200,
+      message: "Blog edited successfully",
+      data: blogPrevDb,
+    });
+  } catch (error) {
+    return res.send({
+      status: 400,
+      message: "Invalid data",
+      error: error,
+    });
+  }
+};
+
+//=========Delete Blog Controller
+const deleteBlogController = async (req, res) => {
+  const { blogId } = req.body;
+  const userId = req.session.user.userId;
+
+  //find the blog
+  try {
+    const blogDb = await getBlogWithId({ blogId });
+
+    //ownership check
+
+    if (!userId.equals(blogDb.userId)) {
+      return res.send({
+        status: 403,
+        message: "You are not allowed to delete",
+      });
+    }
+
+    //delete the blog
+    const deletedBlog = await deleteBlog({ blogId });
+    return res.send({
+      status: 200,
+      message: "Blog deleted successfully",
+      data: deleteBlog,
+    });
+  } catch (error) {
+    return res.send({
+      status: 400,
+      message: "Invalid data",
+      error: error,
+    });
+  }
+  res.send("delete working");
+};
+
+module.exports = {
+  createBlogController,
+  getBlogsController,
+  getMyBlogsController,
+  editBlogController,
+  deleteBlogController,
+};
--- models/blogModel.js
+++ models/blogModel.js
@@ -1,3 +1,4 @@
+const { LIMIT } = require("../privateConstants");
 const blogSchema = require("../schemas/blogSchema");
 
 const createBlog = async ({ title, textBody, userId }) => {
@@ -25,6 +26,12 @@ const getAllBlogs = ({ SKIP }) => {
         {
           $sort: { creationDateTime: -1 }, //-1 DESC, +1 ASCD
         },
+        {
+          $skip: SKIP,
+        },
+        {
+          $limit: LIMIT,
+        },
       ]);
       resolve(blogDb);
     } catch (error) {
@@ -33,4 +40,82 @@ const getAllBlogs = ({ SKIP }) => {
   });
 };
 
-module.exports = { createBlog, getAllBlogs };
+const getmyBlogs = ({ userId, SKIP }) => {
+  return new Promise(async (resolve, reject) => {
+    //match based on only user blogs > sort >pagination
+
+    try {
+      const myblogsDb = await blogSchema.aggregate([
+        {
+          $match: { userId: userId },
+        },
+        {
+          $sort: { creationDateTime: -1 },
+        },
+        {
+          $skip: SKIP,
+        },
+        {
+          $limit: LIMIT,
+        },
+      ]);
+      resolve(myblogsDb);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const getBlogWithId = ({ blogId }) => {
+  return new Promise(async (resolve, reject) => {
+  
+    try {
+      if (!blogId) {
+        reject("Blog id is missing");
+      }
+      const blogDb = await blogSchema.findOne({ _id: blogId });
+      if (!blogDb) {
+        reject(`Blog not found with blogId : ${blogId}`);
+      }
+      resolve(blogDb);
+    } catch (error) {
+      reject(error)
+    }
+  });
+};
+
+// function to Edit the blog in Database
+const editBlog = ({ title, textBody, blogId }) => {
+  return new Promise(async (reslove, reject) => {
+    try {
+      const blogPrevDb = await blogSchema.findOneAndUpdate(
+        { _id: blogId },
+        { title: title, textBody: textBody }
+      );
+      reslove(blogPrevDb);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+//function find and delete the blog from db
+const deleteBlog = ({ blogId }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const deletedBlogDb = await blogSchema.findOneAndDelete({ _id: blogId });
+      resolve(deletedBlogDb);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  createBlog,
+  getAllBlogs,
+  getmyBlogs,
+  getBlogWithId,
+  editBlog,
+  deleteBlog
+};
